6b7beed70568a3360c2cfe204332a1887ae936b9
NirmalJBehanan <nirmalbehanan93@gmail.com>
2026-09-14 10:49:36 +0530

    netlify link added

--- services/mail.service.js
+++ services/mail.service.js
@@ -10,7 +10,7 @@ const brevo = new BrevoClient({
 
 export const sendForgetMail = async(email, token) => {
 
-    const reset = `https://autendication.netlify.app/${token}`;
+    const reset = `https://autendication.netlify.app/reset-password/${token}`;
 
     try {
         await brevo.transactionalEmails.sendTransacEmail({
